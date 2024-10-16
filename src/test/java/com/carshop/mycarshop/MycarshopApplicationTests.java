9dfa06263b2902a456b73ce4f7da5ad48acc0240
choilhyun <cihg155@gmail.com>
2024-10-16 18:22:14 +0900

    [로컬 빌드 실패 처리] ActiveProfile을 활용해서 빌드 처리

---
+++ src/test/java/com/carshop/mycarshop/MycarshopApplicationTests.java
@@ -0,0 +1,15 @@
+package com.carshop.mycarshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("integration")
+class MycarshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
