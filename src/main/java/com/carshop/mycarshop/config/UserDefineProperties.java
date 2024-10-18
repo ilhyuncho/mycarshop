d21a79a3e7dc1afe24ea5d418892cdcf29a94cdf
choilhyun <cihg155@gmail.com>
2024-10-18 17:27:36 +0900

    [컨피그 클라이언트 설정] test 프로파일 로 실행시 원격 설정 서버에서 [설정값] 가져오기(docker는 안됨)

---
+++ src/main/java/com/carshop/mycarshop/config/UserDefineProperties.java
@@ -0,0 +1,18 @@
+package com.carshop.mycarshop.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "welcome")  // @ConfigurationProperties 빈을 통해 사죵자 정의 속성을 정의했기 때문에
+// 새로고침 이벤트인 RefreshScopeRefreshEvent가 발생하면 기본적으로 자동 적용되어
+// 최신 설정 값을 다시 로드 한다
+// http://localhost:9001/actuator/refresh 가 post로 호출 되면 ( 사용자 가 호출 )
+public class UserDefineProperties {
+    private String greeting;
+
+    public String getGreeting(){
+        return this.greeting;
+    }
+    public void setGreeting(String greeting){
+        this.greeting = greeting;
+    }
+}
--- src/main/java/com/carshop/mycarshop/controller/HomeController.java
+++ src/main/java/com/carshop/mycarshop/controller/HomeController.java
@@ -1,13 +1,20 @@
 package com.carshop.mycarshop.controller;
 
+import com.carshop.mycarshop.config.UserDefineProperties;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class HomeController {
+
+
+    private final UserDefineProperties userDefineProperties;
+
     @GetMapping("/")
     public String getGreeting(){
-        return "환영합니다";
+        return userDefineProperties.getGreeting();
     }
 }
 
