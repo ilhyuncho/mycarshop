5d01af97b75bffb55665bcb2a2b84697203e7dc4
choilhyun <cihg155@gmail.com>
2024-12-08 15:23:58 +0900

    [swagger-ui] aws로 올린 서버에서도 모두 접속 가능 하게 ( 임시로 )

--- src/main/java/com/carshop/mycarshop/config/security/SecurityConfig.java
+++ src/main/java/com/carshop/mycarshop/config/security/SecurityConfig.java
@@ -53,14 +53,9 @@ public class SecurityConfig {
                 .antMatchers("/verify/email").permitAll()
                 .antMatchers("/reference/listRefCarType").permitAll()
 
-                .antMatchers(
-                        "/swagger-ui/**",
-                        "/v3/api-docs/**",
-                        "/swagger-ui.html",
-                        "/webjars/**",
-                        "/static/**")
-                .permitAll()
-             //  .antMatchers("/swagger-ui/index.html**").permitAll()        // 임시로
+                // aws 에 올린 서버에서 http://13.124.166.39:8080/swagger-ui/index.html 접속 했을때 오류 처리
+                // message: Refused to apply style from 'http://13.124.166.39:8080/auth/login' because its MIME type ('text/html') is not a supported stylesheet MIME type, and strict MIME checking is enabled.
+
                 //.antMatchers( "/swagger-ui/index.html**").hasRole("ADMIN")
 //                .antMatchers("/myPage/**").permitAll()
                 .anyRequest().authenticated();
