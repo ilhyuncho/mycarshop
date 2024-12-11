f3e559c4697f69c16901fb10ed974747c3b839bb
choilhyun <cihg155@gmail.com>
2024-12-11 16:08:17 +0900

    [healthcheck] rest api 생성

--- src/main/java/com/carshop/mycarshop/controller/HomeController.java
+++ src/main/java/com/carshop/mycarshop/controller/HomeController.java
@@ -2,20 +2,22 @@ package com.carshop.mycarshop.controller;
 
 import com.carshop.mycarshop.config.UserDefineProperties;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-//@RestController
-//@RequiredArgsConstructor
-//public class HomeController {
-//
-//    private final UserDefineProperties userDefineProperties;
-//
-//    @GetMapping("/")
-//    public String getGreeting(){
-//
-//
-//        return userDefineProperties.getGreeting();
-//    }
-//}
+@RestController
+@RequiredArgsConstructor
+public class HomeController {
+
+    private final UserDefineProperties userDefineProperties;
+
+    @GetMapping("/healthcheck")
+    public ResponseEntity<String> getGreeting() {
+
+
+        return ResponseEntity.status(HttpStatus.OK).body("success");
+    }
+}
 
