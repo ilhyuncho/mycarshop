9cb9d0238d6f719314184743e2feff8608b37a1a
choilhyun <cihg155@gmail.com>
2024-12-20 18:39:11 +0900

    [코드 정리]

--- src/main/java/com/carshop/mycarshop/domain/user/UserGradeType.java
+++ src/main/java/com/carshop/mycarshop/domain/user/UserGradeType.java
@@ -10,20 +10,22 @@ import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public enum UserGradeType {
-    GRADE_NONE(0L, "등급 없음"),
-    GRADE_A(1L, "등급 A"),    // 일반 회원
-    GRADE_E(2L, "등급 E"),
-    GRADE_S(3L, "등급 S");
+    GRADE_NONE(0L, "등급 없음", "노 등급"),
+    GRADE_A(1L, "등급 A", "일반 회원"),    // 일반 회원
+    GRADE_E(2L, "등급 E", "우수 회원"),
+    GRADE_S(3L, "등급 S", "VIP 회원");
 
     private final Long type;
     private final String typeName;
+    private final String gardeDesc;
 
     private final static Map<Long, UserGradeType> typeMap = Arrays.stream(UserGradeType.values())
             .collect(Collectors.toMap(UserGradeType::getType, Function.identity()));
 
-    UserGradeType(Long type, String typeName) {
+    UserGradeType(Long type, String typeName, String gardeDesc) {
         this.type = type;
         this.typeName = typeName;
+        this.gardeDesc = gardeDesc;
     }
 
     @JsonCreator
@@ -38,5 +40,6 @@ public enum UserGradeType {
     }
 
     public String getTypeName(){return typeName;}
+    public String getGardeDesc(){return gardeDesc;}
 
 }
