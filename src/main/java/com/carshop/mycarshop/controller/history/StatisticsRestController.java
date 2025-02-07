9ae20499c1eaa1f0bce1583d96b7f454ca64caa2
choilhyun <cihg155@gmail.com>
2025-02-07 10:46:09 +0900

    [리펙토링] if-else 개선 - 함수형 인터페이스 를 활용 Function - apply

--- src/main/java/com/carshop/mycarshop/controller/history/StatisticsRestController.java
+++ src/main/java/com/carshop/mycarshop/controller/history/StatisticsRestController.java
@@ -1,6 +1,5 @@
 package com.carshop.mycarshop.controller.history;
 
-import com.carshop.mycarshop.domain.user.User;
 import com.carshop.mycarshop.dto.statistics.StatisticsReqDTO;
 import com.carshop.mycarshop.dto.statistics.StatisticsResDTO;
 import com.carshop.mycarshop.dto.statistics.StatisticsTotalResDTO;
@@ -20,6 +19,8 @@ import javax.validation.Valid;
 import java.security.Principal;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
 
 @RestController
 @RequestMapping("/statistics")
@@ -35,32 +36,28 @@ public class StatisticsRestController {
     @ApiOperation(value = "내차 통계 내역 조회", notes = "")
     @GetMapping("/get")
     public List<StatisticsResDTO> getCarStatistics(@Valid StatisticsReqDTO statisticsReqDTO,
-                                      BindingResult bindingResult,
-                                      Principal principal){
+                                      BindingResult bindingResult, Principal principal){
 
         userService.findUser(principal.getName());
         carService.getCarInfo(statisticsReqDTO.getCarId());
 
-        List<StatisticsResDTO> listDto = new ArrayList<>();
+        // 함수형 인터페이스 를 활용 Function - apply
+        Map<String, Function<StatisticsReqDTO, List<StatisticsResDTO>>> statisticsFunctionMap =
+                Map.of(
+                    "#consume", carStatisticsService::getStatisticsConsume,
+                    "#fuelAmount", carStatisticsService::getStatisticsFuelAmount,
+                    "#distance", carStatisticsService::getStatisticsDistance
+            );
 
-        if("#consume".equals(statisticsReqDTO.getTargetId())){
-            listDto = carStatisticsService.getStatisticsConsume(statisticsReqDTO);
-        }
-        else if("#fuelAmount".equals(statisticsReqDTO.getTargetId())){
-            listDto = carStatisticsService.getStatisticsFuelAmount(statisticsReqDTO);
-        }
-        else if("#distance".equals(statisticsReqDTO.getTargetId())){
-            listDto = carStatisticsService.getStatisticsDistance(statisticsReqDTO);
-        }
-
-        return listDto;
+        return statisticsFunctionMap
+                .getOrDefault(statisticsReqDTO.getTargetId(), req -> new ArrayList<>())
+                .apply(statisticsReqDTO);
     }
 
     @ApiOperation(value = "내차 통계 총 내역 조회", notes = "")
     @GetMapping("/total")
     public StatisticsTotalResDTO getTotalCarStatistics(@Valid StatisticsReqDTO statisticsReqDTO,
-                                       BindingResult bindingResult,
-                                       Principal principal){
+                                       BindingResult bindingResult, Principal principal){
 
         userService.findUser(principal.getName());
         carService.getCarInfo(statisticsReqDTO.getCarId());
