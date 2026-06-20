e96df57bf8fe55e9556506bcccab099686f9c781
cihg1 <cihg1@naver.com>
2026-06-20 22:42:24 +0900

    [redis]같은 Service 클래스 안에서 @Cacheable 메서드를 부를 때, Spring 프록시를 거치게 수정

--- src/main/java/com/carshop/mycarshop/service/reference/RefCarSampleService.java
+++ src/main/java/com/carshop/mycarshop/service/reference/RefCarSampleService.java
@@ -2,6 +2,10 @@ package com.carshop.mycarshop.service.reference;
 
 import com.carshop.mycarshop.domain.reference.RefCarSample;
 
+import java.util.List;
+
 public interface RefCarSampleService {
     RefCarSample findMyCar(String carNumber);
+
+    List<RefCarSample> getAllRefCarSamples();
 }
--- src/main/java/com/carshop/mycarshop/service/reference/RefCarSampleServiceImpl.java
+++ src/main/java/com/carshop/mycarshop/service/reference/RefCarSampleServiceImpl.java
@@ -1,9 +1,12 @@
 package com.carshop.mycarshop.service.reference;
 
+import com.carshop.mycarshop.config.RedisCacheNames;
 import com.carshop.mycarshop.domain.reference.RefCarSample;
 import com.carshop.mycarshop.domain.reference.RefCarSampleRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -16,26 +19,30 @@ import java.util.Random;
 public class RefCarSampleServiceImpl implements RefCarSampleService {
 
     private final RefCarSampleRepository refCarSampleRepository;
-
+    private final ObjectProvider<RefCarSampleService> refCarSampleServiceProvider;
 
     @Override
     public RefCarSample findMyCar(String carNumber) {
 
-        if(carNumber.isEmpty()){
-            // 임의로 차량 정보 db에서 get ( 테스트 간편하게... )
-            List<RefCarSample> listCarSample = refCarSampleRepository.findAll();
+        if (carNumber.isEmpty()) {
+            // 같은 Service 클래스 안에서 @Cacheable 메서드를 부를 때, Spring 프록시를 거치게 하려고 ObjectProvider.getObject()로 다시 호출한 것
+            List<RefCarSample> listCarSample = refCarSampleServiceProvider.getObject().getAllRefCarSamples();
 
-            if(listCarSample.size() > 0){
+            if (listCarSample.size() > 0) {
                 int skipIndex = new Random().nextInt(listCarSample.size() - 1);
                 return listCarSample.stream().skip(skipIndex).findFirst().get();
-            }
-            else{
+            } else {
                 return null;
             }
-        }
-        else{
+        } else {
             Optional<RefCarSample> byCarNumber = refCarSampleRepository.findByCarNumber(carNumber);
             return byCarNumber.orElse(null);
         }
+    }
+
+    @Override
+    @Cacheable(value = RedisCacheNames.REF_CAR_SAMPLES, key = "'all'")
+    public List<RefCarSample> getAllRefCarSamples() {
+        return refCarSampleRepository.findAll();
     }
 }
